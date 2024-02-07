8096e2d5e86e14c6b88ef6e3dbbf268e50bb548d
CarlosR <carlos.ramos@grupoimportia.com>
2024-02-07 10:09:57 +0800

    update contracts

--- packages/hardhat/contracts/StormBit.sol
+++ packages/hardhat/contracts/StormBit.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.21;
 contract StormBit {
 
     event PoolCreated(address indexed creator, uint256 poolId, string name);
+<<<<<<< HEAD
+=======
+    event AgreementCreated(address indexed aggreement, uint256 agreementId, string name);
+
+>>>>>>> 0163ca7 (Merge branch 'main' of github.com:Quantum3-Labs/StormBit-monorepo)
     struct PoolConfig {
         uint256 minCreditScore;
         uint256 quorum;
@@ -25,6 +30,16 @@ contract StormBit {
     // poolId => (agreementId => YEs/NO)
     mapping(uint256 => mapping(uint256 => bool)) public poolAgreements;
 
+<<<<<<< HEAD
+=======
+    constructor() {
+        _createAgreement("Base Agreement", address(1));
+        _createAgreement("NFT Agreement", address(2));
+        _createAgreement("FT Agreement", address(3));
+
+    }
+
+>>>>>>> 0163ca7 (Merge branch 'main' of github.com:Quantum3-Labs/StormBit-monorepo)
     function createPool(string memory _name, PoolConfig memory _config) public {
         StormPool memory pool = StormPool(msg.sender, 1, _name);
         pools[poolIds] = pool;
@@ -46,14 +61,33 @@ contract StormBit {
         return _agreements;
     }
 
+<<<<<<< HEAD
+=======
+    function createAgreement(string memory _name, address _agreement) public returns (uint256) {
+        return _createAgreement(_name, _agreement);
+    }
+
+    function supportAgreement(uint256 _poolId, uint256 _agreementId) public {
+        _supportAgreement(_poolId, _agreementId);
+    }
+
+>>>>>>> 0163ca7 (Merge branch 'main' of github.com:Quantum3-Labs/StormBit-monorepo)
     function _supportAgreement(uint256 _poolId, uint256 _agreementId) internal {
         poolAgreements[_poolId][_agreementId] = true;
     }
 
+<<<<<<< HEAD
     function _addAgreement(address _agreement) internal returns (uint256) {
         uint256 agreementId = aggreementIds;
         agreements[aggreementIds] = _agreement;
         aggreementIds++;
+=======
+    function _createAgreement(string memory _name, address _agreement) internal returns (uint256) {
+        uint256 agreementId = aggreementIds;
+        agreements[agreementId] = _agreement;
+        aggreementIds++;
+        emit AgreementCreated(_agreement, agreementId, _name);
+>>>>>>> 0163ca7 (Merge branch 'main' of github.com:Quantum3-Labs/StormBit-monorepo)
         return agreementId;
     }
 
